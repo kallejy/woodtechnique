a1d133346b64484d382583884d4f2cee754d00f0
Kalle Nilsson <kalle@fempunkter.se>
2018-08-24 11:37:30 +0200

    Create theme, third try

--- deploy.php
+++ deploy.php
@@ -20,11 +20,12 @@ set('keep_releases', 5);
 set('shared_dirs', ['web/app/uploads']);
 set('shared_files', ['.env']);
 
-// Activate plugin after deploy
-task('activate-plugins', function() {
+// Activate plugins and themes deploy
+task('activate-plugins-and-themes', function() {
   run("cd {{ deploy_path }}/current && wp plugin activate redirection");
+  run("cd {{ deploy_path }}/current && wp theme activate woodtechnique");
 });
-after('deploy', 'activate-plugins');
+after('deploy', 'activate-plugins-and-themes');
 
 // [Optional] if deploy fails automatically unlock.
 after('deploy:failed', 'deploy:unlock');
