50bb770b3e5303258431da28c150a9eb734c2b76
Kalle Nilsson <kalle@fempunkter.se>
2018-08-28 08:44:26 +0200

    Added task pull

--- deploy.php
+++ deploy.php
@@ -22,6 +22,7 @@ set('repository', 'git@github.com:kallejy/woodtechnique.git');
 
 set('env_vars', '/usr/bin/env');
 set('keep_releases', 5);
+set('git_tty', true);
 set('shared_dirs', ['web/app/uploads']);
 set('shared_files', ['.env']);
 
@@ -44,3 +45,30 @@ after('deploy', 'activate-femp');
 
 // If deploy fails automatically unlock
 after('deploy:failed', 'deploy:unlock');
+
+
+// Pull from production
+task('pull', function () {
+  $host = Task\Context::get()->getHost();
+  $user = $host->getUser();
+  $hostname = $host->getHostname();
+  $url = parse_url(run("cd {{deploy_path}}/current && wp config get --constant=WP_HOME"), PHP_URL_HOST);
+  $localUrl = parse_url(runLocally("wp config get --constant=WP_HOME"), PHP_URL_HOST);
+  $actions = [
+      "ssh {$user}@{$hostname} 'cd {{deploy_path}}/current && wp db export - | gzip' > db.sql.gz",
+      "gzip -df db.sql.gz",
+      "vendor/bin/wp language core install sv_SE",
+      "vendor/bin/wp db import db.sql",
+      "rm -f db.sql",
+      "vendor/bin/wp search-replace '{$url}' '{$localUrl}' --all-tables",
+      "rsync --exclude .cache -re ssh " .
+          "{$user}@{$hostname}:{{deploy_path}}/shared/web/app/uploads web/app",
+      "vendor/bin/wp rewrite flush",
+      "vendor/bin/wp cache flush",
+      "vendor/bin/wp theme update --all"
+  ];
+  foreach ($actions as $action) {
+      writeln("{$action}");
+      writeln(runLocally($action, ['timeout' => 999]));
+  }
+})->desc('Get the production setup to your local dev env');
